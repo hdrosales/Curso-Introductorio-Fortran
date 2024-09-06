90efc7893fcc06600969bbe48d7ca6085228c07e
Diego Rosales <rosales@fisica.unlp.edu.ar>
2024-09-05 21:21:44 -0300

    Añadidas nuevas carpetas y archivos

--- 03-Codigos-basicos/ejemplo1.f95
+++
@@ -1,9 +0,0 @@
-PROGRAM ejemplo1
-    !Este programa no hace nada.
-    !Lo anterior es un comentario.
-    print *
-    write(*,*) 'La clase de Fortran comienza a las 16hs. No faltes!!!!'
-    print *
-!    write(*,'(I4,f9.5)')22, 2.345
-END PROGRAM ejemplo1
- 
